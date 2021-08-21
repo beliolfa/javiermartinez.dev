a94d2eb6427c804c55bb95386758a28189aff4c8
Javier Martinez <javi@64robots.com>
2021-08-21 21:11:12 +0200

    fixes

--- pages/es/index.vue
+++ pages/es/index.vue
@@ -1,6 +1,6 @@
 <template>
 <div>
-  <img class="mt-4" src="https://user-images.githubusercontent.com/12644599/92306359-c227bc80-ef8e-11ea-91ba-671002208621.jpg">
+  <img class="my-4" src="https://user-images.githubusercontent.com/12644599/92306359-c227bc80-ef8e-11ea-91ba-671002208621.jpg">
   <h2 class="text-xl text-gray-800 border-b border-gray-300 pb-8">
     <p>Antes que nada, soy un ser humano. Casado con la mejor mujer del mundo y padre de dos de los mas preciosos (y molestos) niños en el universo.</p>
     <p>Despues, soy Lead Developer en <a class="text-blue-800 font-bold" href="https://www.64robots.com">64 Robots</a>. La agencia de software más maravillosa de los Estados Unidos. Hacemos productos chulos con Nuxt, Laravel y Tailwind.</p>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
 <div>
-  <img class="mt-4" src="https://user-images.githubusercontent.com/12644599/92306359-c227bc80-ef8e-11ea-91ba-671002208621.jpg">
+  <img class="my-4" src="https://user-images.githubusercontent.com/12644599/92306359-c227bc80-ef8e-11ea-91ba-671002208621.jpg">
   <h2 class="text-xl text-gray-800 border-b border-gray-300 pb-8">
     <p>First of all, I'm a person. Married to the best woman in the whole planet and father of two of the most beautiful (and annoying) children in the universe.</p>
     <p>Then I'm Lead Developer at <a class="text-blue-800 font-bold" href="https://www.64robots.com">64 Robots</a>. The most kickass software agency on the United States. We do cool products with Nuxt, Laravel and Tailwind.</p>
